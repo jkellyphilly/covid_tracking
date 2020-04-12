44963a6dc15f55be201630e5c0ebad1e62bdc5f8
Joel Kelly <joel.kelly.philly@gmail.com>
2020-04-12 22:53:28 +0000

    adding summary's #print_all method

--- lib/covid_tracking/summary.rb
+++ lib/covid_tracking/summary.rb
@@ -19,4 +19,15 @@ class CovidTracking::Summary
     self.all.clear
   end
   
+  def self.print_all
+    self.all.each do |summary|
+      puts "#{summary.name}:"
+      puts "    New Confirmed Cases: #{summary.new_confirmed}"
+      puts "    Total Confirmed Cases: #{summary.total_confirmed}"
+      puts "    New Deaths: #{summary.new_deaths}"
+      puts "    Total Deaths: #{summary.total_deaths}"
+      puts "    New Recovered Cases: #{summary.new_recovered}"
+      puts "    Total Recovered Cases: #{summary.total_recovered}"
+      puts "This information was last updated at #{summary.date}"
+    end
 end
