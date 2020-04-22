8b8cadd08319e382f4ff18e27356598f48de58c2
jkellyphilly <joel.kelly.philly@gmail.com>
2020-04-22 15:51:49 -0400

    cleaned up Summary class

--- lib/covid_tracking/summary.rb
+++ lib/covid_tracking/summary.rb
@@ -1,24 +1,24 @@
 # Parent class for all summaries
 
 class CovidTracking::Summary
-  
-  attr_accessor :name, :new_confirmed, :total_confirmed, :new_deaths, :total_deaths, :new_recovered, :total_recovered, :date 
-  
+
+  attr_accessor :name, :new_confirmed, :total_confirmed, :new_deaths, :total_deaths, :new_recovered, :total_recovered, :date
+
   @@all = []
-  
+
   def initialize(attributes)
     attributes.each {|key, value| self.send("#{key}=", value)}
     self.class.all << self
   end
-  
-  def self.all 
-    @@all 
-  end 
-  
-  def self.reset 
+
+  def self.all
+    @@all
+  end
+
+  def self.reset
     self.all.clear
   end
-  
+
   def self.print_all
     self.all.each do |summary|
       puts "#{summary.name}:".light_cyan
